f6e3b0ba7d3269bfd7250cf96264e6eff12c427b
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 18:27:06 +0200

    init checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_associations',
+    version='1.0',
+    packages=['pygate_associations'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="plugin for the pygate: create associations between devices connected to the gateway"
+)
